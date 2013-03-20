fa695efc58bb2db5c2321c5ce1c23892fb4440d5
Rebecca Lovewell <rebecca@caktusgroup.com>
2013-03-20 16:23:12 -0400

    Fixes #10 - Added initial model migrations

---
+++ nutrition/migrations/0001_initial.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Report'
+        db.create_table(u'nutrition_report', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('updated_date', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('status', self.gf('django.db.models.fields.CharField')(default=u'U', max_length=1, null=True, blank=True)),
+            ('reporter_id', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('patient_id', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('global_reporter_id', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('global_patient_id', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('height', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=4, decimal_places=1, blank=True)),
+            ('weight', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=4, decimal_places=1, blank=True)),
+            ('muac', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=4, decimal_places=1, blank=True)),
+            ('oedema', self.gf('django.db.models.fields.NullBooleanField')(default=None, null=True, blank=True)),
+            ('weight4age', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=4, decimal_places=2, blank=True)),
+            ('height4age', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=4, decimal_places=2, blank=True)),
+            ('weight4height', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=4, decimal_places=2, blank=True)),
+        ))
+        db.send_create_signal(u'nutrition', ['Report'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Report'
+        db.delete_table(u'nutrition_report')
+
+
+    models = {
+        u'nutrition.report': {
+            'Meta': {'object_name': 'Report'},
+            'created_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'global_patient_id': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'global_reporter_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'height': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '4', 'decimal_places': '1', 'blank': 'True'}),
+            'height4age': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '4', 'decimal_places': '2', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'muac': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '4', 'decimal_places': '1', 'blank': 'True'}),
+            'oedema': ('django.db.models.fields.NullBooleanField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
+            'patient_id': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'reporter_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "u'U'", 'max_length': '1', 'null': 'True', 'blank': 'True'}),
+            'updated_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'weight': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '4', 'decimal_places': '1', 'blank': 'True'}),
+            'weight4age': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '4', 'decimal_places': '2', 'blank': 'True'}),
+            'weight4height': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '4', 'decimal_places': '2', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['nutrition']
